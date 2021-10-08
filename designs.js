fc6d5ab6b148cb978d3675f9a539725bcacc3fd5
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-08 09:35:00 +0300

    initial

--- designs.js
+++ designs.js
@@ -3,8 +3,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
+// ___________________________________
+// Roadmap101:
+// 1. Create a grid dynamically(?);
+// 2. SElect each  individual cell an assign  it to the colorPicker(?);
+// 
+// ====================================================
+
+const numberOfRows = document.getElementById('inputHeight');
+const numberOfColumns = document.getElementById('inputWidth');
+
+numberOfRows.addEventListener('input',function(){
+    console.log('The number of rows input');
+})
+const numberOfColumns = document.addEventListener('input',function(){
+    console.log('The number of columns input');
+})
+// ----------------------------------------
+
 function makeGrid() {
 
-// Your code goes here!
-
-}
+    // Your code goes here!
+    
+    }

95e0d185cb3415fcf3314f66904eea26a2bd6f3b
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-08 12:19:12 +0300

    roadmap 101

--- designs.js
+++ designs.js
@@ -3,23 +3,35 @@
 
 // When size is submitted by the user, call makeGrid()
 
-// ___________________________________
-// Roadmap101:
-// 1. Create a grid dynamically(?);
-// 2. SElect each  individual cell an assign  it to the colorPicker(?);
-// 
-// ====================================================
-
+    // ___________________________________
+        // Roadmap101:
+        // 1. Create a grid dynamically(?);
+        // 2. SElect each  individual cell an assign  it to the colorPicker(?);
+        // =====================================
+        //Getahold of  inputs and form elements:
+        // ====================================================
+        // 1. number of rows:
 const numberOfRows = document.getElementById('inputHeight');
+        // 2. number of columns:
 const numberOfColumns = document.getElementById('inputWidth');
+        // 3. colorPicker:
+const pickTheColor = document.getElementById('colorPicker');
+    // --------------------------
 
+        // adding eventListener:
+        // 1.
 numberOfRows.addEventListener('input',function(){
     console.log('The number of rows input');
 })
-const numberOfColumns = document.addEventListener('input',function(){
+        // 2.
+numberOfColumns.addEventListener('input',function(){
     console.log('The number of columns input');
 })
-// ----------------------------------------
+        // 3.
+pickTheColor.addEventListener('click',function(){
+    console.log('Pick the color here!')
+})
+    // ----------------------------------------
 
 function makeGrid() {
 

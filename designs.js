6cb2545ab0f74d6181db6ef9adb258ab8260142d
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-09 18:33:26 +0300

    grid 2x2

--- designs.js
+++ designs.js
@@ -11,31 +11,68 @@
         //Getahold of  inputs and form elements:
         // ====================================================
         // 1. number of rows:
-const numberOfRows = document.getElementById('inputHeight');
-        // 2. number of columns:
-const numberOfColumns = document.getElementById('inputWidth');
-        // 3. colorPicker:
-const pickTheColor = document.getElementById('colorPicker');
-    // --------------------------
+// const numberOfRows = document.getElementById('inputHeight').value;
+//         // 2. number of columns:
+// const numberOfColumns = document.getElementById('inputWidth').value;
+//         // 3. colorPicker:
+// const pickTheColor = document.getElementById('colorPicker').value;
+//         // 4. colorPicker:
+// const customGridColor = document.getElementById('pixelCanvas');
+//         // 5. create grid:  
+// const customGridSize = document.getElementById('sizePicker');
 
-        // adding eventListener:
-        // 1.
-numberOfRows.addEventListener('input',function(){
-    console.log('The number of rows input');
-})
-        // 2.
-numberOfColumns.addEventListener('input',function(){
-    console.log('The number of columns input');
-})
-        // 3.
-pickTheColor.addEventListener('click',function(){
-    console.log('Pick the color here!')
-})
-    // ----------------------------------------
+//     // --------------------------
+//     // --------------------------
+//     console.log(numberOfRows);
+//         // adding eventListener:
+//         // 1.
+// numberOfRows.addEventListener('input',function(){
+       
+// })
+//         // 2.
+// numberOfColumns.addEventListener('input',function(){
+//     console.log('The number of columns input');
+// })
+//         // 3.
+// pickTheColor.addEventListener('click',function(){
+//     console.log('Pick the color here!')
+// })
+//     // ----------------------------------------
 
-const customGrid = document.getElementById('pixelCanvas');
+
 
 function makeGrid() {
     // Your code goes here!
-    
-    }
+// get the reference for the body
+var body = document.getElementsByTagName('body')[0];
+
+// creates a <table> element and a <tbody> element
+var grid = document.createElement('grid');
+var gridBody = document.createElement('gbody');
+
+//creating cells
+for (var r = 0; r < 2; r++){
+        //creates a table row
+        var row = document.createElement('tr');
+
+        for (var c = 0 ; c < 2; c++){
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+            var cell = document.createElement('td');
+            var cellText = document.createTextNode('cell in row '+r+', column '+c);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        // add the row to the end of the grid body
+        gridBody.appendChild(row);
+}
+// put the gbody in the grid
+grid.appendChild(gridBody);
+// appends grid into body
+body.appendChild(grid);
+// sets the border attribut of grid to 2
+grid.setAttribute('border','2');   
+}
+
+makeGrid();
